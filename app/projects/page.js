94bd59d5dd86a8d618708549c319ac58778fbdae
Abdul Mateen <mateen.2k19@gmail.com>
2024-08-06 22:50:53 +0500

    Liniting errors fixed

--- app/projects/page.js
+++ app/projects/page.js
@@ -4,27 +4,6 @@ import Page from "components/utility/Page";
 import Projects from "components/projects/Projects";
 import React from "react";
 
-export const metadata = {
-  title: "Projects - Mateen",
-  description:
-    "I love coding using tools like React, NextJS, Tailwind, and many more! Here are some of my favorite projects.",
-  alternates: {
-    canonical: "https://miteshtagadiya.js.org/projects"
-  },
-  openGraph: {
-    title: "Projects - Mateen",
-    description:
-      "I love coding using tools like React, NextJS, Tailwind, and many more! Here are some of my favorite projects.",
-    url: "https://miteshtagadiya.js.org/projects"
-  },
-  twitter: {
-    title: "Projects - Mateen",
-    description:
-      "I love coding using tools like React, NextJS, Tailwind, and many more! Here are some of my favorite projects.",
-    url: "https://miteshtagadiya.js.org/projects"
-  }
-};
-
 function projects() {
   return (
     <Page
