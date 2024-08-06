99feb82fc8cbb1429e591ebfa5603a23faf41aa4
Abdul Mateen <mateen.2k19@gmail.com>
2024-08-06 22:17:43 +0500

    About section added

--- components/main/index.js
+++ components/main/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Projects from "@/components/home/Projects";
 import Skills from "@/components/home/Skills";
+import About from "@/components/home/About";
 import Testimonials from "@/components/home/Testimonials";
 
 function Main() {
   return (
     <div className="mt-20 space-y-32" id="main-section">
+      <About/>
       <Projects />
       <Skills />
       <Testimonials />
