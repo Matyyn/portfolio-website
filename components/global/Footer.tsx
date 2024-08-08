98aceb773f715f34fa3eae5a75708e4d9e4f4256
Abdul Mateen <mateen.2k19@gmail.com>
2024-08-08 21:38:58 +0500

    Footer Socials Layout Updated

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -7,10 +7,10 @@ import Image from "next/image";
 function Footer() {
   return (
     <footer className="flex flex-col w-screen px-5 py-10 border-t border-fun-pink-darker z-5 bg-bg">
-      <div className="w-full max-w-4xl m-auto grid grid-cols-2 sm:grid-cols-2 justify-between items-start">
+      <div className="w-full max-w-4xl m-auto grid grid-cols-1 sm:grid-cols-2 justify-center items-start">
         {footer.columns.map((item, index) => {
           return (
-            <div key={index} className="text-left mb-5 sm:mb-0">
+            <div key={index} className="text-center mb-5 sm:mb-0">
               <p className="uppercase text-fun-gray text-sm font-bold">
                 {item.title}
               </p>
@@ -22,7 +22,7 @@ function Footer() {
                         <a
                           href={item.link}
                           target="_blank"
-                          className="items-center flex"
+                          className="items-center flex justify-center"
                         >
                           {item.icon && (
                             <span className="pr-2 -mb-1">
@@ -46,50 +46,6 @@ function Footer() {
             </div>
           );
         })}
-        {/* <div className="text-center col-span-2 sm:col-auto sm:text-left pt-8 sm:mt-0 sm:pt-0 text-fun-gray border-t border-fun-pink-dark sm:border-0">
-          <p className="uppercase text-fun-gray text-sm font-bold">
-            Support My Work
-          </p>
-          <div className="space-y-2 mt-4 w-full flex items-center sm:items-start flex-col">
-            {footer.support.buymeacoffee !== "" && (
-              <div>
-                <a
-                  href={`https://buymeacoffee.com/${footer.support.buymeacoffee}`}
-                  target="_blank"
-                >
-                  <Image
-                    alt="buy-me-a-coffee"
-                    src={`${process.env.NEXT_PUBLIC_BASE_URL}static/misc/buy-me-a-coffee.svg`}
-                    className="h-12 mr-2 hover:opacity-80 opacity-100 transition-opacity"
-                    height={30}
-                    width={100}
-                    style={{ width: "100%" }}
-                  />
-                </a>
-              </div>
-            )}
-            {footer.support.paypal !== "" && (
-              <div>
-                <a
-                  href={`https://paypal.me/${footer.support.paypal}`}
-                  target="_blank"
-                >
-                  <Image
-                    alt="paypal"
-                    src={`${process.env.NEXT_PUBLIC_BASE_URL}static/misc/paypal.svg`}
-                    className="h-12 mr-2 hover:opacity-80 opacity-100 transition-opacity"
-                    height={30}
-                    width={100}
-                    style={{ width: "100%" }}
-                  />
-                </a>
-              </div>
-            )}
-            <p className="text-fun-gray text-xs pt-1">
-              {footer.support.message}
-            </p>
-          </div>
-        </div> */}
       </div>
       <div className="max-w-4xl w-full m-auto mt-8 pt-8 sm:mt-4 sm:pt-4 text-center text-fun-gray border-t border-fun-pink-dark">
         <div className="flex flex-col items-center justify-center ">
@@ -129,7 +85,7 @@ function Footer() {
               </span>
             </div>
           </div>
-     
+
         </div>
       </div>
     </footer>
